84baae0ad51036c773c0195e0deb9c9ebe7a8df6
Maicon Strey <mstrey@gmail.com>
2025-08-25 14:21:07 -0300

    Create Trensurb.php

---
+++ empresas/Trensurb.php
@@ -0,0 +1,21 @@
+class Trensurb extends Empresa {
+
+    protected string $url = 'https://sisop.trensurb.gov.br/App/getSituacaoOperacional.php?user=trensite&senha=h1ATPtmrKiOxe3qC';
+
+    protected function obterDados() {
+        $json = @file_get_contents($this->url);
+        return $json ? json_decode($json, true) : null;
+    }
+
+    protected function isAlteracaoDeServico(mixed $dados) {
+        if (!isset($dados['situacao'])) {
+            return "Erro: Estrutura de dados da Trensurb não encontrada.";
+        }
+
+        // A situação 'Em Operação Normal' é o status 'normal'.
+        if ($dados['situacao'] !== 'Em Operação Normal') {
+            return "Situação: {$dados['situacao']} - Descrição: {$dados['descricao']}";
+        }
+        return false;
+    }
+}

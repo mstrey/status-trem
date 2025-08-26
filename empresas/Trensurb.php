3a66276d485074ef85bcf5d2c938310494d57049
Maicon Strey <mstrey@gmail.com>
2025-08-26 14:53:57 -0300

    Update Trensurb.php

--- empresas/Trensurb.php
+++ empresas/Trensurb.php
@@ -5,19 +5,20 @@ class Trensurb extends Empresa {
 
     protected string $url = 'https://sisop.trensurb.gov.br/App/getSituacaoOperacional.php?user=trensite&senha=h1ATPtmrKiOxe3qC';
 
+    protected const ARR_SITUACAO_NORMAL = [4];
+    
     protected function obterDados() {
         $json = @file_get_contents($this->url);
         return $json ? json_decode($json, true) : null;
     }
 
     protected function isAlteracaoDeServico(mixed $dados) {
-        if (!isset($dados['situacao'])) {
-            return "Erro: Estrutura de dados da Trensurb não encontrada.";
+        if (!isset($dados['status-situacao-operacional'])) {
+            return "Erro: Estrutura de dados da Trensurb não encontrada.\n" . print_r($dados, true);
         }
 
-        // A situação 'Em Operação Normal' é o status 'normal'.
-        if ($dados['situacao'] !== 'Em Operação Normal') {
-            return "Situação: {$dados['situacao']} - Descrição: {$dados['descricao']}";
+        if (!in_array($dados['status-situacao-operacional'], self::ARR_SITUACAO_NORMAL)) {
+            return "Situação: {$dados['situacao']} - Descrição: {$dados['descricao']} - {$dados['motivo']}";
         }
         return false;
     }
